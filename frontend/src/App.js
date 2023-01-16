73be387470bafd75c9878941cad42408ffaf40a9
Stian Lindhom <stian@lindhom.no>
2023-01-16 12:04:25 +0100

    Basic login functionality with Spring Security

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,17 @@ import './App.css';
 import {useEffect, useState} from "react";
 function App() {
   const [message, setMessage] = useState("Loading message...")
+  const [login, setLogin] = useState(false)
   useEffect(() => {
     const loadData = async () => {
-      const res = await fetch('/api/greet')
-      const greeting = await res.text()
-      setMessage(greeting)
+      const res = await fetch('/api/greet', {redirect: "manual"})
+      if (res.status === 0) {
+        setLogin(true)
+        setMessage('Could not load message - perhaps not logged in?')
+      } else {
+        const greeting = await res.text()
+        setMessage(greeting)
+      }
     }
 
     loadData()
@@ -16,6 +22,12 @@ function App() {
   return (
     <div className="App">
       <h1>{message}</h1>
+      {login ?
+      <form method='post' action='/login'>
+        <input type='text' name='username' placeholder='Username' />
+        <input type='text' name='password' placeholder='Password' />
+        <input type='submit' name='login' value='Login' />
+      </form> : (<></>)}
     </div>
   );
 }
---
+++ src/main/java/com/example/sample/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.sample;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and().
+                formLogin();
+
+        http.csrf().disable();
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("123").roles("USER").build());
+        return manager;
+    }
+}
