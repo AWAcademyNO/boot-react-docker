32245706eb1a07978dbc362cd2ea75ddb7167b19
Stian Lindhom <stian@lindhom.no>
2023-01-16 09:27:54 +0100

    Load message from backend (proxied)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useEffect, useState} from "react";
 function App() {
+  const [message, setMessage] = useState("Loading message...")
+  useEffect(() => {
+    const loadData = async () => {
+      const res = await fetch('/api/greet')
+      const greeting = await res.text()
+      setMessage(greeting)
+    }
+
+    loadData()
+        .catch(console.error)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{message}</h1>
     </div>
   );
 }
